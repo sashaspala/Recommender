f1d0eaf38c500f2b1ac4ac54f83fd3bef7636100
meghanahickey <meghanahickey@gmail.com>
2016-12-08 16:43:48 -0500

    Commented out unnecessary predictions

--- matrix_factorization.py
+++ matrix_factorization.py
@@ -15,16 +15,17 @@ rank = 10
 numIterations = 10
 model = ALS.train(ratings, rank, numIterations)
 
+# I don't think the two following two blocks of code are necessary
 # Evaluate the model on training data
-testdata = ratings.map(lambda p: (p[0], p[1]))
-predictions = model.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
-ratesAndPreds = ratings.map(lambda r: ((r[0], r[1]), r[2])).join(predictions)
-MSE = ratesAndPreds.map(lambda r: (r[1][0] - r[1][1])**2).mean()
-print("Mean Squared Error = " + str(MSE))
+# testdata = ratings.map(lambda p: (p[0], p[1]))
+# predictions = model.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
+# ratesAndPreds = ratings.map(lambda r: ((r[0], r[1]), r[2])).join(predictions)
+# MSE = ratesAndPreds.map(lambda r: (r[1][0] - r[1][1])**2).mean()
+# print("Mean Squared Error = " + str(MSE))
 
 # Save and load model
-model.save(data_file, "target/tmp/myCollaborativeFilter")
-sameModel = MatrixFactorizationModel.load(data_file, "target/tmp/myCollaborativeFilter")
+# model.save(data_file, "target/tmp/myCollaborativeFilter")
+# sameModel = MatrixFactorizationModel.load(data_file, "target/tmp/myCollaborativeFilter")
 
 features_matrix = model.productFeatures()
 features_dict = {}
